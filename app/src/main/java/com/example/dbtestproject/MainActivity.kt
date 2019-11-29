24558ea61c39190e881a980a64c3f86aeeb4189b
mwshubham <mwshubham@gmail.com>
2019-11-29 14:56:58 +0530

    POC for https://stackoverflow.com/questions/2493331/what-are-the-best-practices-for-sqlite-on-android/2493839#2493839

--- app/src/main/java/com/example/dbtestproject/MainActivity.kt
+++ app/src/main/java/com/example/dbtestproject/MainActivity.kt
@@ -2,9 +2,10 @@ package com.example.dbtestproject
 
 import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,71 +21,46 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         dbHelper = FeedReaderDbHelper(this)
-        insert()
+
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-
-            val db = dbHelper.readableDatabase
-
-// Define a projection that specifies which columns from the database
-// you will actually use after this query.
-            val projection = arrayOf(
-//                BaseColumns._ID,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
-                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE
-            )
-
-// Filter results WHERE "title" = 'My Title'
-            val selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE
-
-// How you want the results sorted in the resulting Cursor
-            val sortOrder = "${FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE} DESC"
-
-            val cursor = db.query(
-                FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
-                projection,             // The array of columns to return (pass null to get all)
-                null,              // The columns for the WHERE clause
-                null,          // The values for the WHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                null               // The sort order
-            )
-
-            cursor.use {
-                it ?: return@setOnClickListener
-                if (it.count >= 0) {
-                    it.moveToFirst()
-                    while (cursor.moveToNext()) {
-                        Toast.makeText(
-                            applicationContext,
-                            "${it.getString(0)} >>> ${it.getString(1)}",
-                            Toast.LENGTH_SHORT
-                        )
-                            .show()
-                    }
-
+            Thread {
+                for (i in 1..5) {
+                    insert()
                 }
-
-            }
+            }.start()
+            Thread {
+                for (i in 1..5) {
+                    insert()
+                }
+            }.start()
         }
     }
 
     private fun insert() {
+        Log.e(TAG, "insert() " + Thread.currentThread().name)
         // Gets the data repository in write mode
-        val db = dbHelper.writableDatabase
-
+        val db = dbHelper.readableDatabase
+        try {
 // Create a new map of values, where column names are the keys
-        val values = ContentValues().apply {
-            put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "Title: " + Random.nextInt(0, 1000))
-            put(
-                FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE,
-                "Subtitle: " + Random.nextInt(0, 1000)
-            )
-        }
+            val values = ContentValues().apply {
+                put(
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
+                    "Title: " + Random.nextInt(0, 1000) + " " + Thread.currentThread().name
+                )
+                put(
+                    FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE,
+                    "Subtitle: " + Random.nextInt(0, 1000) + " " + Thread.currentThread().name
+                )
+            }
 
 // Insert the new row, returning the primary key value of the new row
-        val newRowId = db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
+            val newRowId = db?.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values)
+        } catch (e: Exception) {
+            Log.e(TAG, "insert() " + e.message)
+        } finally {
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -100,6 +76,50 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+    }
+
+    fun viewData(view: View) {
+        val db = dbHelper.readableDatabase
+
+// Define a projection that specifies which columns from the database
+// you will actually use after this query.
+        val projection = arrayOf(
+//                BaseColumns._ID,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE,
+            FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE
+        )
+
+// Filter results WHERE "title" = 'My Title'
+        val selection = FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE
+
+// How you want the results sorted in the resulting Cursor
+        val sortOrder = "${FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE} DESC"
+
+        val cursor = db.query(
+            FeedReaderContract.FeedEntry.TABLE_NAME,   // The table to query
+            projection,             // The array of columns to return (pass null to get all)
+            null,              // The columns for the WHERE clause
+            null,          // The values for the WHERE clause
+            null,                   // don't group the rows
+            null,                   // don't filter by row groups
+            null               // The sort order
+        )
+
+        cursor.use {
+            it ?: return
+            if (it.count >= 0) {
+                it.moveToFirst()
+                do {
+                    Log.e(TAG, "${it.getString(0)} >>> ${it.getString(1)}")
+                } while (cursor.moveToNext())
+
+            }
+
         }
     }
 }
